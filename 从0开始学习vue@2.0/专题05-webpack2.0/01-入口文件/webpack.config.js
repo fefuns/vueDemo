7f0b90eb91a09d0d0bbe58e95c3a0b95c9e3be5f
hezhi <1321120469@qq.com>
2017-06-15 16:40:45 +0800

    单入口文件

---
+++ 从0开始学习vue@2.0/专题05-webpack2.0/01-入口文件/webpack.config.js
@@ -0,0 +1,11 @@
+const config = {
+    // 入口起点
+    entry: './main.js',
+    // 输出文件
+    output: {
+        // 输出文件名
+        filename: 'bundle.js'
+    }
+};
+
+module.exports = config;
---
+++ 从0开始学习vue@2.0/专题05-webpack2.0/README.md
@@ -0,0 +1,7 @@
+本专题示例基于webpack@2.6.1
+
+学习资料：
+
+1.[阮一峰webpack-demos中文版](https://github.com/viviannow/webpack-ruanyf) <br>
+2.[webpack2.2中文文档](http://www.css88.com/doc/webpack2/)
+
