3e55bf17e0c04c5ba1037b83a8386cd743346360
hezhi <1321120469@qq.com>
2017-06-15 16:44:51 +0800

    webpack专题-babel-loader 加载器

---
+++ 从0开始学习vue@2.0/专题05-webpack2.0/03-babel-loader 加载器/es6.js
@@ -0,0 +1,4 @@
+var f = v => v;
+var sum = (num1, num2) => num1 + num2;
+
+console.log(sum(1, 2));
---
+++ 从0开始学习vue@2.0/专题05-webpack2.0/03-babel-loader 加载器/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    entry: './es6.js',
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules | bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015']
+                    }
+                }
+            }
+        ]
+    }
+}
