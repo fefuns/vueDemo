798b26ee3f901978cee51fdff53df875ed7e4f0e
hezhi <1321120469@qq.com>
2017-06-19 15:03:10 +0800

    webpack2.x专题: file-loader 与 url-loader来编译图片

---
+++ 从0开始学习vue@2.0/专题05-webpack2.0/06-url-loader 编译图片/webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: './main.js',
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8192
+                    }
+                }
+            }
+        ]
+    }
+}
