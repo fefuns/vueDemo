6d1f00acee9a2fc0368f65c52baf2da7be29c795
hezhi <1321120469@qq.com>
2017-06-29 13:56:37 +0800

    vuex专题-基础小例子

---
+++ 从0开始学习vue@2.0/专题08-通过vue-cli深入理解vuex/01-basic/src/components/Count.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>{{ msg }}</h2>
+    <hr>
+    <h3>{{ $store.state.count }}</h3>
+    <p>
+      <button @click="$store.commit('add')">+</button>
+      <button @click="$store.commit('reduce')">-</button>
+    </p>
+  </div>
+</template>
+<script>
+  import store from '@/vuex/store'
+  export default {
+    data() {
+      return {
+        msg: 'Hello Vuex'
+      }
+    },
+    store
+  }
+</script>
---
+++ 从0开始学习vue@2.0/专题08-通过vue-cli深入理解vuex/readme.md
@@ -0,0 +1,3 @@
+## 学习笔记
+
+1. [01-basic](http://note.youdao.com/noteshare?id=39d0f4ccfb16349a776ddef82b39be7c&sub=E20F8911F2C9422A969BE3D2409CEAB8)
